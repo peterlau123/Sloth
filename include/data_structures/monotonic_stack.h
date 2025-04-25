c4f057a62ca0c1f5fc4558ee456285dec8aa76ee
liuxin.image <liuxin.image@bytedance.com>
2025-04-25 08:54:40 +0800

    feat: add leetcode reference

--- include/data_structures/monotonic_stack.h
+++ include/data_structures/monotonic_stack.h
@@ -3,7 +3,7 @@
 namespace sloth {
 
   namespace stack {
-
+    //refer to https://leetcode.com/problems/verify-preorder-sequence-in-binary-search-tree/?envType=study-plan-v2&envId=premium-algo-100
     class MonotonicStack {};
 
   }  // namespace stack
